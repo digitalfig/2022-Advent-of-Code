6143f4a08c85ba26efee7852550b6e2eb5638322
digitalfig <digitalfig@users.noreply.github.com>
2022-12-11 18:34:40 -0500

    Add files via upload

--- AoC Day02/aoc_day02.py
+++ AoC Day02/aoc_day02.py
@@ -1,6 +1,10 @@
 ## Advent of Code 2022 -- Day 02:
-## Day 2: Rock Paper Scissors
+'''Day 2: Rock Paper Scissors
 
+The input file is your full tournament, and has each match on a line. Your opponent's hand-shape is first (A, B or C) and your response is represented in second (X, Y or Z). In each match you score 1 for throwing Rock, 2 for Paper or 3 for Sissors, and you also score an added 6 for winning, 3 for a draw or 0 for losing the match.
+'''
+
+## READ INPUT FILE:
 def import_file( test ):
 	if test.upper() == 'TEST':
 		fn = 'test.txt'
@@ -19,7 +23,7 @@ def score_it(lines, score_array):
 		score += 1 + score_array.index(l)
 	return score
 
-# PART 1: --------------
+'''PART 1: Your response X, Y or Z represents respectively the Rock, Paper or Scissors hand-shape you show in each match.'''
 #Rock    =  A, X  =  1
 #Paper   =  B, Y  =  2
 #Sissors =  C, Z  =  3
@@ -30,7 +34,7 @@ score_array = ["BX", "CY", "AZ", "AX", "BY", "CZ", "CX", "AY", "BZ"]
 score = score_it(lines, score_array)
 print("Score Part 1:", score)
 
-# PART 2: ---------------
+'''PART 2: Your response (X, Y or Z) represents if you lose, draw or win the match, respectively.'''
 #lose = X = 0 +    "AX": 3, "BX": 1, "CX": 2
 #draw = Y = 3 +    "AY": 1, "BY": 2, "CY": 3
 #win  = Z = 6 +    "AZ": 2, "BZ": 3, "CZ": 1
